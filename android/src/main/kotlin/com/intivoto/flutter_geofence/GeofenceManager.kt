282719a925f228a934382b0a8eace9e8d661d129
Marco Lettieri <m.lettieri@microbees.com>
2022-02-16 14:15:47 +0100

    fix for android 11

--- android/src/main/kotlin/com/intivoto/flutter_geofence/GeofenceManager.kt
+++ android/src/main/kotlin/com/intivoto/flutter_geofence/GeofenceManager.kt
@@ -4,6 +4,7 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.location.Location
+import android.os.Build
 import android.os.Looper
 import android.renderscript.RenderScript
 import android.util.Log
@@ -66,7 +67,11 @@ class GeofenceManager(context: Context,
 
     private val geofencePendingIntent: PendingIntent by lazy {
         val intent = Intent(context, GeofenceBroadcastReceiver::class.java)
-        PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE)
+        } else {
+            PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
     }
 
 
